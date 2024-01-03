79aa15c0677646bc047c295d6fd0e4f955703525
douglasd11 <dguerreroe@unicartagena.edu.co>
2024-01-03 04:30:17 -0500

    maquetado de header

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,37 @@ import { useState } from 'react'
 
 import './App.css'
 
+import discord from './assets/discord.svg'
+import github from './assets/github.svg'
+import linkedin from './assets/linkedin.svg'
+
 function App() {
 
   return (
     <>
-      <h2>LudoNex</h2>
+      <header className='header'>
+        <div className='cont-header'>
+          <div className='principal'>
+            <h2>ludonex</h2>
+            <div className='menu'>
+              <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Proyectos</a></li>
+                <li><a href="#">Contactos</a></li>
+              </ul>
+            </div>
+          </div>
+          
+          <div className='redes'>
+            <div><img src={discord} /></div>
+            <div><img src={github} /></div>
+            <div><img src={linkedin} /></div>
+          </div>
+        </div>
+      </header>
+
+      {/* <h2>LudoNex</h2> */}
     </>
   )
 }
